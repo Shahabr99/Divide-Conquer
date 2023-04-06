e60dfbf28e24ed3c691e14d8400b241a4511083a
Shahabr99 <sean.rohani9@gmail.com>
2023-04-06 13:11:05 -0700

    last one

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -5,8 +5,8 @@ function findRotatedIndex(arr, val) {
 }
 
 // module.exports = findRotatedIndex;
-console.log(findRotatedIndex([3, 4, 1, 2], 4)); // 1
-console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)); // 2
+findRotatedIndex([3, 4, 1, 2], 4); // 1
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8); // 2
 findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3); // 6
 findRotatedIndex([37, 44, 66, 102, 10, 22], 14); // -1
 findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12); // -1
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,14 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  let minIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[minIndex]) {
+      minIndex = i;
+    }
+  }
+  return minIndex;
 }
 
-module.exports = findRotationCount
+// module.exports = findRotationCount
+console.log(findRotationCount([15, 18, 2, 3, 6, 12])); // 2
+findRotationCount([7, 9, 11, 12, 5]); // 4
+findRotationCount([7, 9, 11, 12, 15]); // 0
